ea567ed677ab0d17ea7956095b8db291e87e3dcd
竹本隼人 <takehaya1026@gmail.com>
2018-06-03 00:14:34 +0900

    pr_report02.pyのコメントアウト修正

--- pr_report02.py
+++ pr_report02.py
@@ -3,6 +3,7 @@ import networkx as nx
 
 G = nx.DiGraph()
 
+# node作成 edge作成
 G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 G.add_edges_from([
 	(2, 1), (3, 1), (4, 1),
@@ -13,10 +14,10 @@ G.add_edges_from([
 	(8, 6), (8, 2)
 ])
 
-# pagerank計算 ランダムサーフ率デフォルト0.85
+# pagerank計算 ランダムサーフ率デフォルト0.15
 mapping_pr = nx.pagerank(G)
 
-# label付け替え 小数点4位以下切り捨て
+# label付け替え 小数点4位以下切り上げ
 mapping_pr = { k:"[%s] %s" % (k, round(v, 4)) for (k, v) in mapping_pr.items() }
 G = nx.relabel_nodes(G, mapping_pr)
 
@@ -27,5 +28,7 @@ nx.draw_networkx_labels(G, pos, fontsize=14, font_weight="bold")
 nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='C')
 
 plt.axis('off')
+
+# 画像保存
 plt.savefig("./images/pr_report02.png")
 plt.show()
